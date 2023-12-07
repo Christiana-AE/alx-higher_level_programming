de2cde4a3d4952a60a920970ff68f83e91d00574
Christiana-AE <christine.anako@gmail.com>
2023-12-07 22:37:30 +0000

    Data structures

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    new_array = []
+    count = 0
+    for i in range(len(my_list)):
+        if my_list[i] not in new_array:
+            new_array.append(my_list[i])
+            continue
+
+    for i in range(len(new_array)):
+        count += new_array[i]
+    return count
+ 
